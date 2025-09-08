ba7b9f70c4c344c82f443e30090b43cc1bf74a9d
Tia Piol <tiarox02@yahoo.com>
2025-09-08 14:17:16 -0700

    render the page

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('shoes', views.shoe_index, name='shoe-index'),
 ]
